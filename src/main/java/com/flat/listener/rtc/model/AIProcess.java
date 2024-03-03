aa96b91c137fdd58b67618711644414c08fa6dfa
koojawon <jawonkoo@naver.com>
2024-03-03 14:06:50 +0900

    -feat: AI프로세스 생성, 데이터 publisher

---
+++ src/main/java/com/flat/listener/rtc/model/AIProcess.java
@@ -0,0 +1,77 @@
+package com.flat.listener.rtc.model;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.SubmissionPublisher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AIProcess {
+
+    private final SubmissionPublisher<String> publisher;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+
+    public AIProcess() {
+        publisher = new SubmissionPublisher<>();
+    }
+
+    public void start() {
+        try {
+            Process process = Runtime.getRuntime().exec("programName");
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+            bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        } catch (IOException e) {
+            log.error("child process start failed!! : {}", e.getMessage());
+        }
+    }
+
+    public void writeData(byte[] data) {
+        try {
+            
+            char[] buf = new char[data.length];
+            for (int i = 0; i < buf.length; i++) {
+                buf[i] = (char) data[i];
+            }
+
+            bufferedWriter.write(buf);
+        } catch (IOException e) {
+            log.error("Write error!! : {}", e.getMessage());
+        }
+    }
+
+    public void startRead() {
+        Thread thread = new Thread(
+                () -> {
+                    String s;
+                    while (true) {
+                        try {
+                            if ((s = bufferedReader.readLine()) == null) {
+                                break;
+                            }
+                            if (!s.isEmpty()) {
+                                publisher.submit(s);
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+
+    public void stopRead() {
+        publisher.close();
+    }
+
+    public Publisher<String> asPublisher() {
+        return publisher;
+    }
+}
