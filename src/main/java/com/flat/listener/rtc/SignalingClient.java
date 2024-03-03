f58d1e2fcd186cee55f9a79a979757b7d16dc99f
koojawon <jawonkoo@naver.com>
2024-03-03 13:59:09 +0900

    -refactor: send함수를 분명하게 분리 (타입 검사하지 않게)

--- src/main/java/com/flat/listener/rtc/SignalingClient.java
+++ src/main/java/com/flat/listener/rtc/SignalingClient.java
@@ -1,12 +1,16 @@
 package com.flat.listener.rtc;
 
 import com.flat.listener.rtc.model.Contact;
+import dev.onvoid.webrtc.RTCIceCandidate;
+import dev.onvoid.webrtc.RTCSessionDescription;
 
 public interface SignalingClient {
 
     void joinRoom(Contact asContact) throws Exception;
 
-    void send(Contact contact, Object obj, String uuid) throws Exception;
+    void sendSdpOffer(Contact contact, RTCSessionDescription obj, String uuid) throws Exception;
+
+    void sendIceCandidate(final RTCIceCandidate iceCandidate, String uuid);
 
     void setSignalingListener(SignalingListener listener);
 
