2e9d44590d0a34bf244131eceeddb2d40c99b0aa
koojawon <jawonkoo@naver.com>
2024-03-03 14:01:38 +0900

    -refactor: 미사용 메소드 삭제

--- src/main/java/com/flat/listener/rtc/model/PeerConnectionContext.java
+++ src/main/java/com/flat/listener/rtc/model/PeerConnectionContext.java
@@ -1,23 +1,10 @@
 package com.flat.listener.rtc.model;
 
-import dev.onvoid.webrtc.RTCPeerConnectionState;
 import dev.onvoid.webrtc.RTCRtpTransceiverDirection;
-import dev.onvoid.webrtc.RTCStatsReport;
-import dev.onvoid.webrtc.media.video.VideoFrame;
 import java.util.UUID;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class PeerConnectionContext {
+    public final String uniqueId = UUID.randomUUID().toString();
     public RTCRtpTransceiverDirection audioDirection;
-    public String uniqueId = UUID.randomUUID().toString();
-
-    public Consumer<RTCStatsReport> onStatsReport;
-
-    public BiConsumer<Contact, VideoFrame> onRemoteFrame;
-
-    public BiConsumer<Contact, Boolean> onRemoteVideoStream;
-
-    public BiConsumer<Contact, RTCPeerConnectionState> onPeerConnectionState;
 
 }
