e5f0e9d638d9aaefb1d9be4c423c3c061040bf66
koojawon <jawonkoo@naver.com>
2024-07-07 15:07:10 +0900

    -refactor 로깅/argument 변경

--- src/main/java/com/flat/listener/rtc/model/AIProcess.java
+++ src/main/java/com/flat/listener/rtc/model/AIProcess.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.SubmissionPublisher;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,6 @@ public class AIProcess {
     public Thread thread;
     private BufferedReader bufferedReader;
     private Process process;
-    private OutputStream e;
 
     public AIProcess() {
         publisher = new SubmissionPublisher<>();
@@ -25,32 +25,28 @@ public class AIProcess {
 
     public void start(String fileName) {
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder("python",
-                    "C:/Users/jawon/Downloads/flat_test/argparse.py");
+            ProcessBuilder processBuilder = new ProcessBuilder("python3",
+                    "//wsl.localhost/Ubuntu-20.04/home/blue/flat/flat.py", "--score_midi_path",
+                    "//wsl.localhost/Ubuntu-20.04/home/blue/flat/data/25-12/25-12.mid", "--mode", "stream", "--backend",
+                    "timestamp", "--backend_output", "stdout");
+            processBuilder.redirectError(Redirect.INHERIT);
             process = processBuilder.start();
             if (process.isAlive()) {
                 log.info("successfully started, state : {}", process.isAlive());
-                e = process.getOutputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 startRead();
             }
         } catch (Exception e) {
-            e.printStackTrace();
             log.error("child process start failed!! : {}", e.getMessage());
         }
     }
 
     public synchronized void writeData(byte[] data) {
-        if (e != null) {
-            try {
-                log.info("Writing data to the stream.");
-                e.write(data);
-                // e.flush();
-            } catch (IOException ex) {
-                log.error("Failed to write data: {}", ex.getMessage(), ex);
-            }
-        } else {
-            log.error("OutputStream not initialized or closed.");
+        try {
+            OutputStream e = process.getOutputStream();
+            e.write(data);
+            e.flush();
+        } catch (IOException ex) {
+            log.error("Failed to write data: {}", ex.getMessage(), ex);
         }
     }
 
@@ -58,25 +54,26 @@ public class AIProcess {
         thread = new Thread(
                 () -> {
                     String s;
+                    bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                     while (true) {
                         try {
-                            if ((s = bufferedReader.readLine()) == null) {
-                                break;
+                            s = bufferedReader.readLine();
+                            if (s != null) {
+                                this.publisher.submit(s);
                             }
-                            publisher.submit(s);
                         } catch (IOException e) {
-                            e.printStackTrace();
                             throw new RuntimeException(e);
                         }
                     }
                 }
         );
         thread.start();
+        log.info(String.valueOf(thread.isAlive()));
     }
 
     public void stopRead() {
+
         process.destroy();
-        thread.interrupt();
         publisher.close();
     }
 
